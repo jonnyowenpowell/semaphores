27162d4fec1c5b4d070dca1942c0c0b3fec5a0ff
Jonny Powell <jonnyowenpowell@gmail.com>
2021-05-16 18:36:42 +0100

    Added python test script.

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ This project uses the [Gradle](https://gradle.org/) build tool.
 - On Windows, run `gradlew.bat run`.
 
 _Note_: This command automatically builds prior to running.
+
+## Testing
+
+To test the program for correctness, a Python script is included,
+along with a Gradle task to run the script. You will need Python 3.6
+or higher installed and available on your PATH as `python3`.
+
+The test script first runs the Java program to collect the output,
+and then performs checks on the output for correctness.
+
+To run the test script:
+- On *nix systems or WSL, run `./gradlew checkOutput`.
+- On Windows, run `gradlew.bat checkOutput`.
---
+++ scripts/check_output.py
@@ -0,0 +1,47 @@
+import os
+
+JAR_NAME = 'semaphores-1.0.0.jar'
+
+# Make sure to run `gradlew build` before using this script
+
+def get_program_output():
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    jar_directory = os.path.abspath(os.path.join(script_directory,
+        '../build/libs'))
+    os.chdir(jar_directory)
+
+    output_stream = os.popen(f'java -jar {JAR_NAME}')
+    output = output_stream.read()
+
+    return output
+
+def check_output(output):
+    w_count, y_count, z_count = 0, 0, 0
+    w_expected, y_expected = True, True
+
+    try:
+        for c in output:
+            if c in ['W', 'X']:
+                assert w_expected == (c == 'W')
+                w_expected = not w_expected
+
+            if c in ['y', 'z']:
+                assert y_expected == (c == 'y')
+                y_expected = not y_expected
+
+            if c == 'W':
+                w_count += 1
+            elif c == 'y':
+                y_count += 1
+            elif c == 'z':
+                z_count += 1
+            assert w_count >= y_count + z_count
+
+        print('Program output passed checks 🎉')
+    except AssertionError:
+        print('Program output failed checks 😔')
+
+if __name__ == '__main__':
+    program_output = get_program_output()
+    print(f'Program output was:\n{program_output}')
+    check_output(program_output)
