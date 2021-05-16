8d35d6385f031d7f80c67180bff4121877ed473a
Jonny Powell <jonnyowenpowell@gmail.com>
2021-05-16 17:51:51 +0100

    Added reduced version of final solution.

---
+++ src/main/java/semaphores/RunReduced.java
@@ -0,0 +1,36 @@
+package semaphores;
+
+public class RunReduced {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Initialise semaphores
+        // These semaphores ensure W and X alternate in the output
+        var wSemaphore = new BinarySemaphore(true);
+        var xSemaphore = new BinarySemaphore(false);
+
+        // Initialise CharacterPrinter Runnables
+        // These W and X printers have a limit of 100 each.
+
+        var wPrinter = new CharacterPrinter('W',
+            new Semaphore[] { wSemaphore },
+            new Semaphore[] { xSemaphore }, 100);
+        var xPrinter = new CharacterPrinter('X',
+            new Semaphore[] { xSemaphore },
+            new Semaphore[] { wSemaphore }, 100);
+
+        // Initialise and start threads for each CharacterPrinter
+        var wThread = new Thread(wPrinter);
+        var xThread = new Thread(xPrinter);
+        wThread.start();
+        xThread.start();
+
+        // Ensure the program does not exit before all
+        // CharacterPrinter threads have run to completion
+        wThread.join();
+        xThread.join();
+
+        // Exit with code 0
+        System.exit(0);
+    }
+
+}
